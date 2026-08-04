d775a6d0036e87e48876f01e0a52c0db077f311f
mnm <mNmDeveloper@proton.me>
2026-08-04 12:41:33 +0300

    Show activation screen before main app

--- DarkTunnel/DarkTunnelApp.swift
+++ DarkTunnel/DarkTunnelApp.swift
@@ -3,12 +3,19 @@ import SwiftUI
 @main
 struct DarkTunnelApp: App {
     @StateObject private var viewModel = VPNViewModel()
+    @AppStorage("hasCompletedActivation") private var hasCompletedActivation = false
 
     var body: some Scene {
         WindowGroup {
-            HomeView()
-                .environmentObject(viewModel)
-                .preferredColorScheme(.dark)
+            Group {
+                if hasCompletedActivation {
+                    HomeView()
+                        .environmentObject(viewModel)
+                } else {
+                    ActivationView()
+                }
+            }
+            .preferredColorScheme(.dark)
         }
     }
 }
