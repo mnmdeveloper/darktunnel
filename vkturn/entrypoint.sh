9f8cfb557abe48cc18e5f1754a620ed2ea296b0e
mnm <mNmDeveloper@proton.me>
2026-08-05 15:49:16 +0300

    Stream VK Turn logs normally

--- vkturn/entrypoint.sh
+++ vkturn/entrypoint.sh
@@ -13,4 +13,4 @@ sysctl -w net.ipv4.ip_forward=1 >/dev/null
 iptables -C FORWARD -i wg0 -j ACCEPT 2>/dev/null || iptables -A FORWARD -i wg0 -j ACCEPT
 iptables -C FORWARD -o wg0 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT 2>/dev/null || iptables -A FORWARD -o wg0 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 iptables -t nat -C POSTROUTING -s 10.79.0.0/24 -o eth0 -j MASQUERADE 2>/dev/null || iptables -t nat -A POSTROUTING -s 10.79.0.0/24 -o eth0 -j MASQUERADE
-exec /usr/local/bin/vk-turn-proxy -listen 0.0.0.0:56100 -connect 127.0.0.1:51820 -srtp -logfile /dev/stdout
+exec /usr/local/bin/vk-turn-proxy -listen 0.0.0.0:56100 -connect 127.0.0.1:51820 -srtp
