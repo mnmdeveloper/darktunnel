3d0457f03421bf6aefeb13d510e46d0cb0fc4be9
mnm <mNmDeveloper@proton.me>
2026-08-07 13:35:18 +0300

    fix: don't fail vkturn startup on sysctl read-only error

--- vkturn/entrypoint.sh
+++ vkturn/entrypoint.sh
@@ -9,7 +9,7 @@ ip link show wg0 >/dev/null 2>&1 || ip link add wg0 type wireguard
 ip address show dev wg0 | grep -q '10.79.0.1/24' || ip address add 10.79.0.1/24 dev wg0
 wg set wg0 private-key /data/server.key listen-port 51820
 ip link set wg0 up
-sysctl -w net.ipv4.ip_forward=1 >/dev/null
+sysctl -w net.ipv4.ip_forward=1 >/dev/null 2>&1 || true
 iptables -C FORWARD -i wg0 -j ACCEPT 2>/dev/null || iptables -A FORWARD -i wg0 -j ACCEPT
 iptables -C FORWARD -o wg0 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT 2>/dev/null || iptables -A FORWARD -o wg0 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 iptables -t nat -C POSTROUTING -s 10.79.0.0/24 -o eth0 -j MASQUERADE 2>/dev/null || iptables -t nat -A POSTROUTING -s 10.79.0.0/24 -o eth0 -j MASQUERADE
