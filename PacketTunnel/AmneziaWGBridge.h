75c77486e369c6928f41b6ec797be13450453f4c
mnm <mNmDeveloper@proton.me>
2026-08-09 23:07:54 +0300

    Add C declarations for AmneziaWG iOS bridge

---
+++ PacketTunnel/AmneziaWGBridge.h
@@ -0,0 +1,22 @@
+#ifndef DarkTunnel_AmneziaWGBridge_h
+#define DarkTunnel_AmneziaWGBridge_h
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void awgSetLogger(uintptr_t context, uintptr_t loggerFn);
+int32_t awgTurnOn(const char *settings, int32_t tunFd);
+void awgTurnOff(int32_t tunnelHandle);
+int64_t awgSetConfig(int32_t tunnelHandle, const char *settings);
+void awgBumpSockets(int32_t tunnelHandle);
+void awgDisableSomeRoamingForBrokenMobileSemantics(int32_t tunnelHandle);
+char *awgVersion(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
