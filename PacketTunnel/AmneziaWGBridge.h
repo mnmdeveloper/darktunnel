8741d9a2c0a74365466727e15f935dccaef9b568
mnm <mNmDeveloper@proton.me>
2026-08-10 15:14:08 +0700

    Namespace AmneziaWG bridge symbols

--- PacketTunnel/AmneziaWGBridge.h
+++ PacketTunnel/AmneziaWGBridge.h
@@ -2,33 +2,21 @@
 #define DarkTunnel_AmneziaWGBridge_h
 
 #include <stdint.h>
-#include <stdbool.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-// amneziawg-apple's WireGuardKitGo bridge exports the standard WireGuard
-// C symbols (wg*). Keep the awg* names used by DarkTunnel as compatibility
-// aliases so PacketTunnel does not need to know about the vendor naming.
-void wgSetLogger(void *context, void (*loggerFn)(void *context, int level, const char *msg));
-int wgTurnOn(const char *settings, int32_t tunFd);
-void wgTurnOff(int tunnelHandle);
-int64_t wgSetConfig(int tunnelHandle, const char *settings);
-void wgBumpSockets(int tunnelHandle);
-void wgDisableSomeRoamingForBrokenMobileSemantics(int tunnelHandle);
-const char *wgVersion(void);
+void awgSetLogger(uintptr_t context, uintptr_t loggerFn);
+int32_t awgTurnOn(const char *settings, int32_t tunFd);
+void awgTurnOff(int32_t tunnelHandle);
+int64_t awgSetConfig(int32_t tunnelHandle, const char *settings);
+void awgBumpSockets(int32_t tunnelHandle);
+void awgDisableSomeRoamingForBrokenMobileSemantics(int32_t tunnelHandle);
+char *awgVersion(void);
 
 #ifdef __cplusplus
 }
 #endif
-
-#define awgSetLogger wgSetLogger
-#define awgTurnOn wgTurnOn
-#define awgTurnOff wgTurnOff
-#define awgSetConfig wgSetConfig
-#define awgBumpSockets wgBumpSockets
-#define awgDisableSomeRoamingForBrokenMobileSemantics wgDisableSomeRoamingForBrokenMobileSemantics
-#define awgVersion wgVersion
 
 #endif
