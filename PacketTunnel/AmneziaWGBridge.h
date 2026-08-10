050f34e309a81c8b2449e249fd5d5cd3c81cfb92
mnm <mNmDeveloper@proton.me>
2026-08-10 17:39:37 +0700

    Integrate real AmneziaWG iOS backend

--- PacketTunnel/AmneziaWGBridge.h
+++ PacketTunnel/AmneziaWGBridge.h
@@ -11,6 +11,7 @@ void awgSetLogger(uintptr_t context, uintptr_t loggerFn);
 int32_t awgTurnOn(const char *settings, int32_t tunFd);
 void awgTurnOff(int32_t tunnelHandle);
 int64_t awgSetConfig(int32_t tunnelHandle, const char *settings);
+char *awgGetConfig(int32_t tunnelHandle);
 void awgBumpSockets(int32_t tunnelHandle);
 void awgDisableSomeRoamingForBrokenMobileSemantics(int32_t tunnelHandle);
 char *awgVersion(void);
