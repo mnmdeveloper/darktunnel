4eb0590e8addd0837e0ae7c8aeba7aa12b6d02bb
mnm <mNmDeveloper@proton.me>
2026-08-14 13:28:00 +0700

    Fix Android Kotlin compiler target configuration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,14 +1,38 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
 }
 
-android { namespace = "app.lavender3512.darktunnel"; compileSdk = 35
-    defaultConfig { applicationId = "app.lavender3512.darktunnel"; minSdk = 26; targetSdk = 35; versionCode = 1; versionName = "0.1.0" }
-    buildFeatures { compose = true; buildConfig = true }
-    compileOptions { sourceCompatibility = JavaVersion.VERSION_17; targetCompatibility = JavaVersion.VERSION_17 }
-    kotlinOptions { jvmTarget = "17" }
+android {
+    namespace = "app.lavender3512.darktunnel"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "app.lavender3512.darktunnel"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+
+    buildFeatures {
+        compose = true
+        buildConfig = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
+    }
 }
 
 dependencies {
